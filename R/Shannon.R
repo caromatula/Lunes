2357c248156afc7816c31d81efcd710ccc8d1161
Caro Matula <caromatula@gmail.com>
2018-07-16 16:58:40 -0300

    con richness

--- R/Shannon.R
+++ R/Shannon.R
@@ -1,3 +1,14 @@
+#' Calculate shannon index
+#'
+#' @param species vector of species abundances
+#'
+#' @return shannoon index
+#' @export
+#'
+#' @examples 
+#' datos<-c(10, 200, 1000, 50)
+#' shannon(datos)
+#' 
 shannon <- function(species)
 {
   p<-species/sum(species) #calcula proporcion
@@ -7,3 +18,8 @@ shannon <- function(species)
   return(s)
 
 }  
+
+richness<- function(species) 
+  {
+  
+}
